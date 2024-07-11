2a0cae7e52b27f40e31801671175d2fb437d8a27
manojsankar07 <manoj07112003@gmail.com>
2024-07-11 15:15:15 +0530

    Add files via upload

---
+++ sign/index.js
@@ -0,0 +1,9 @@
+var a =prompt("enter first value:");
+var b =prompt("enter second value:");
+var c =prompt("enter third value:");
+var product = a*b*c;
+if(product < 0){
+    alert("the sign is -");
+}else{
+    alert("the sign is +")
+}
