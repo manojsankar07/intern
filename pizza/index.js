6218ac5991206b5800f00bfc5c12ca9534ef82f0
manojsankar07 <manoj07112003@gmail.com>
2024-07-11 14:13:08 +0530

    Add files via upload

---
+++ pizza/index.js
@@ -0,0 +1,10 @@
+var anchoviesPizzaScore = 0;
+var pepperoniPizzaScore = 4;
+var hawaiianPizzaScore = 4;
+var chickenBaconRanchPizzaScore = 6;
+
+console.log(anchoviesPizzaScore < pepperoniPizzaScore); // 1
+console.log(chickenBaconRanchPizzaScore > hawaiianPizzaScore); // 2
+console.log(hawaiianPizzaScore > pepperoniPizzaScore); // 3
+console.log(pepperoniPizzaScore <= chickenBaconRanchPizzaScore); // 4
+console.log(hawaiianPizzaScore >= pepperoniPizzaScore); // 5
