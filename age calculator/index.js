e4e718bea48960ddf212dbc181f04c28e0fdc004
manojsankar07 <manoj07112003@gmail.com>
2024-07-11 14:12:23 +0530

    Add files via upload

---
+++ age calculator/index.js
@@ -0,0 +1,6 @@
+var birthYear = prompt("enter the birth year");
+var futureYear = prompt("enter the future year"); 
+var age1 = futureYear - birthYear;
+var age2 = age1 - 1;
+console.log("I will be either " +age2+" or "+age1+ " in " +futureYear);
+
